c7e6fd9ab4aab7b541ba4aff0d7e7483503541cb
s22025 <s22025@std.it-college.ac.jp>
2023-02-14 07:40:12 +0000

    2.7 about, blog

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import Hero from 'components/hero'
+
+const About = () => {
+	return(
+		<Hero />
+	)
+}
+
+export default About
---
+++ pages/blog/index.js
@@ -0,0 +1,9 @@
+import Hero from 'components/hero'
+
+const Blog = () => {
+	return(
+		<Hero />
+	)
+}
+
+export default Blog
