86eff7dc4ae538d27435b296028146c9a207c85b
s22025 <s22025@std.it-college.ac.jp>
2023-02-15 07:43:47 +0000

    5.9 ソーシャルリンクメニュー

--- components/contact.js
+++ components/contact.js
@@ -1,9 +1,11 @@
+import Social from 'components/social'
 import s from 'styles/contact.module.css'
 
 const Contact = () => {
 	return(
 		<div className={s.stack}>
 			<h3 className={s.heading}>Contact</h3>
+			<Social />
 			<address>cube@web.mail.address</address>
 		</div>
 	)
