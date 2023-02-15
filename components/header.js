c20a00927fbccf646769697c7fd4fdedfc050b2f
s22025 <s22025@std.it-college.ac.jp>
2023-02-15 05:37:36 +0000

    4.1 util.module.css, 4.2 header layout

--- components/header.js
+++ components/header.js
@@ -1,10 +1,14 @@
 import Logo from 'components/logo'
 import Nav from 'components/nav'
+import s from 'styles/header.module.css'
+
 const Header = () => {
         return(
                 <header>
-                	<Logo boxOn />
-			<Nav />
+			<div className={s.flexContainer}>
+	                	<Logo boxOn />
+				<Nav />
+			</div>
 		</header>
         )
 }
