158d664177040429a62ae2601365d638be6fcdf8
s22025 <s22025@std.it-college.ac.jp>
2023-02-15 05:15:54 +0000

    3.5 hero.module.css

--- components/hero.js
+++ components/hero.js
@@ -1,9 +1,13 @@
+import s from 'styles/hero.module.css'
+
 const Hero = ({ title, subtitle, imageOn = false }) => {
         return(
                 <main>
-                        <h1>{title}</h1>
-			<p>{subtitle}</p>
-			{imageOn && <figure> [画像] </figure>}
+			<div className={s.text}>
+	                        <h1 className={s.title}>{title}</h1>
+				<p className={s.subtitle}>{subtitle}</p>
+				{imageOn && <figure> [画像] </figure>}
+			</div>
 		</main>
         )
 }
