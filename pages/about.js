94a8141f04275b2520bf770583c1cf0f14bc6974
s22025 <s22025@std.it-college.ac.jp>
2023-02-16 05:35:58 +0000

    6.3 meta & error fixed

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,4 @@
+import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
 import PostBody from 'components/post-body'
@@ -9,6 +10,7 @@ import Contact from 'components/contact'
 const About = () => {
 	return(
 		<Container>
+			<Meta pageTitle="アバウト" />
 			<Hero 
 				title="About"
 				subtitle="About development activities"
