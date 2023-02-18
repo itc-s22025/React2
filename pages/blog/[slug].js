ba59d63dca48577c85e428a553fdb67712bcdef9
s22025 <s22025@std.it-college.ac.jp>
2023-02-18 16:25:10 +0000

    9.8 ページネーションによる遷移でのブラー

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -30,6 +30,7 @@ export default function Post({ title, publish, content, eyecatch, categories, de
 				
 				<figure>
 					<Image
+						key={eyecatch.url}
 						src={eyecatch.url}
 						alt=""
 						layout="responsive"
