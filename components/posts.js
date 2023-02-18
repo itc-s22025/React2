280ed796571ab0cc4fec2f10881038cb1aef47a2
s22025 <s22025@std.it-college.ac.jp>
2023-02-18 14:16:54 +0000

    9.6 error

--- components/posts.js
+++ components/posts.js
@@ -1,12 +1,24 @@
 import s from 'styles/posts.module.css'
 import Link from 'next/link'
+import Image from 'next/image'
 
 const Posts = ({ posts }) => {
 	return(
 		<div className={s.gridContainer}>
-			{posts.map(({ title,slug }) => (
+			{posts.map(({ title, slug, eyecatch }) => (
 				<article className={s.post} key={slug}>
 					<Link href={`/blog/${slug}`}>
+						<figure>
+							<Image 
+								src={eyecatch.url}
+								alt=""
+								layout="responsive"
+								width={eyecatch.width}
+								height={eyecatch.height}
+								placeholder="blur"
+								blurDataURL={eyecatch.blurDataURL}
+							/>
+						</figure>
 						<h2>{title}</h2>
 					</Link>
 				</article>
