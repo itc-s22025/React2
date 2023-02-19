b4121f8b0f2ec481b755c33a773b73e39be4e49c
s22025 <s22025@std.it-college.ac.jp>
2023-02-19 10:38:35 +0000

    10.3 ハンバーガーメニュー

--- components/nav.js
+++ components/nav.js
@@ -1,23 +1,49 @@
+import { useState } from 'react'
 import Link from 'next/link'
 import s from 'styles/nav.module.css'
 
 const Nav = () => {
+	const [navIsOpen, setNavIsOpen] = useState(false)
+
+	const toggleNav = () => {
+		setNavIsOpen((prev) => !prev)
+	}
+
+	const closeNav = () => {
+		setNavIsOpen(false)
+	}
 	return(
-		<nav>
+		<nav className={navIsOpen ? s.open : s.close}>
+			{navIsOpen && (
+				<style jsx global>{`
+					@media (max-width: 767px){
+						body{
+							overflow: hidden;
+							position: fixed;
+							width: 100%;
+						}
+					}
+				`}</style>
+			)}
+
+			<button className={s.btn} onClick={toggleNav}>
+				<span className={s.bar}></span>
+				<span className="sr-only">MENU</span>
+			</button>
 			<ul className={s.list}>
 				<li>
 					<Link href="/">
-						Home
+						<span onClick={closeNav}>Home</span>
 					</Link>
 				</li>
 				<li>
 					<Link href="/about">
-						About
+						<span onClick={closeNav}>About</span>
 					</Link>
 				</li>
 				<li>
 					<Link href="/blog">
-						Blog
+						<span onClick={closeNav}>Blog</span>
 					</Link>
 				</li>
 			</ul>
